de1aea028b337b5a515d3efbae5cd1ae28977aa6
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-05-16 13:59:24 -0300

    a

--- Pagina Inicial/script.js
+++ Pagina Inicial/script.js
@@ -1,11 +1,10 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
-    if(menuMobile.classList.contains('open')){
+    if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src="imagens" -- colocar a imagem--
-    }else{
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets\img\close_white_36dp.svg";
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
     }
-    
 }
