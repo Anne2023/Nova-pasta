b878cdbe998ca7a7373392a9ddacf47b43b61558
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-05-16 21:04:30 -0300

    a

---
+++ Linguagem de Programação/scriptLG.js
@@ -0,0 +1,23 @@
+button.addEventListener('click', function(){
+    var card = document.querySelector('.card');
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')){
+        return button.textContent = 'Menos';
+    }
+    button.textContent = 'Saiba Mais';
+}
+);
+
+var button =document.getElementById('read_button')
+
+button.addEventListener('click', function(){
+    var card = document.querySelector('.card');
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')){
+        return button.textContent = 'Menos';
+    }
+    button.textContent = 'Saiba Mais';
+}
+);
