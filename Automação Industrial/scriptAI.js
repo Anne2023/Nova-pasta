7c53aee2b62daa171375652a21ef396634c20d8c
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-06-07 14:35:25 -0300

    comit

--- Automação Industrial/scriptAI.js
+++ Automação Industrial/scriptAI.js
@@ -1,14 +1,3 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
-}
-var button =document.getElementById('read_button')
 
 button.addEventListener('click', function(){
     var card = document.querySelector('.card');
