e94bc0613869b9f306e300b7298ed614bf807ba3
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-06-12 17:05:51 -0300

    falta pouco

--- Redes de Computadores/scriptRC.js
+++ Redes de Computadores/scriptRC.js
@@ -9,14 +9,3 @@ function menuShow() {
     }
 }
 var button =document.getElementById('read_button')
-
-button.addEventListener('click', function(){
-    var card = document.querySelector('.card');
-    card.classList.toggle('active');
-
-    if (card.classList.contains('active')){
-        return button.textContent = 'Menos';
-    }
-    button.textContent = 'Saiba Mais';
-}
-);
