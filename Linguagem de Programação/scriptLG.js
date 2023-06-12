ad061a0c444647724efc6731577f1cacd95e9938
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-06-12 14:50:50 -0300

    perfect

--- Linguagem de Programação/scriptLG.js
+++ Linguagem de Programação/scriptLG.js
@@ -1,32 +1,4 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
-    }
-}
-var button =document.getElementById('read_button')
-
-button.addEventListener('click', function(){
-    var card = document.querySelector('.card');
-    card.classList.toggle('active');
-
-    if (card.classList.contains('active')){
-        return button.textContent = 'Menos';
-    }
-    button.textContent = 'Saiba Mais';
-}
-);
-
-document.getElementById("meuLink").addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log("Link clicado, mas o comportamento padrão foi prevenido.");
-});
-
-var slideshow = document.querySelector('.slideshow'); 
+var slideshow = document.querySelector('.slideshow'); /*imagens*/
 var images = slideshow.getElementsByTagName('img');
 var index = 0;
 
@@ -35,3 +7,15 @@ setInterval(function() {
   index = (index + 1) % images.length;
   images[index].style.display = 'inline-block';
 }, 3000);
+
+var slideshow = document.querySelector('.slideshow');/* video*/
+var videos = slideshow.getElementsByTagName('video');
+var index = 0;
+
+setInterval(function() {
+  videos[index].style.display = 'none';
+  index = (index + 1) % videos.length;
+  videos[index].style.display = 'inline-block';
+  videos[index].play();
+}, 5000);
+
