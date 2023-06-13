bd2031cd217cb3098edf90ee6adee4dd42e6a1c8
Amábile Silva <123522722+Anne2023@users.noreply.github.com>
2023-06-12 21:45:08 -0300

    terminando

--- teste/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("saibaMaisBtn").addEventListener("click", function() {
-    document.getElementById("overlay").style.display = "block";
-});
-
-document.getElementById("fecharBtn").addEventListener("click", function() {
-    document.getElementById("overlay").style.display = "none";
-});
